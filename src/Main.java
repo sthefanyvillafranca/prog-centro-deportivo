75350b930685d1a86e6b11aa84bef497fd74346e
sthefanyvillafranca <svillsei@myuax.com>
2026-03-10 01:10:55 +0100

    feat(Main): creacion de objetos e impresion

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        CentroDeportivo centro = new CentroDeportivo();
+        CentroDeportivo centro = new CentroDeportivo(15);
 
-        //  3 entrenadores
-        Entrenador entrenador1 = new Entrenador(1, "Juan Gonzale", "Queseyo", 5);
+        Entrenador entrenador1 = new Entrenador(1, "Juan Gonzalez", "Musculacion", 5);
         Entrenador entrenador2 = new Entrenador(2, "Luis Alfonso", "Cardio", 3);
         Entrenador entrenador3 = new Entrenador(3, "Kelly Jack", "Pilates", 8);
 
         centro.registrarEntrenador(entrenador1);
         centro.registrarEntrenador(entrenador2);
         centro.registrarEntrenador(entrenador3);
-        System.out.println("registro de entrenadores.\n");
 
-        // Mostrar entrenadores
-        System.out.println("Lista de entrenadores: ");
         System.out.println(centro.mostrarEntrenadores());
+        System.out.println("===============<3================");
+        System.out.println("Número total de entrenadores:");
+        System.out.println(centro.contarEntrenadores());
 
+        System.out.println("\n==============<3=================");
+        System.out.println("Buscar un entrenador existente:");
+        System.out.println(centro.buscarEntrenador(2));
+
+        System.out.println("\n================<3===============");
+        System.out.println("Buscar un entrenador inexistente:");
+        System.out.println(centro.buscarEntrenador(4));
     }
 }
